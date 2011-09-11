430074961bba13c7e8c9840480759dde04b0ad75
Darklg <kev.rocher@gmail.com>
2011-09-11 11:08:34 +0200

    fixes #1 Déplacement du choix du séparateur dans sort_css + réindentation des fichiers touchés.

--- inc/functions.php
+++ inc/functions.php
@@ -1,64 +1,65 @@
 <?php
-function clean_css($buffer){
-	// Suppression des commentaires
-	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
 
-	// Suppression des tabulations, espaces multiples, retours à la ligne, etc.
-	$buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '	 ', '	 '), '', $buffer);
+function clean_css($buffer) {
+    // Suppression des commentaires
+    $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
 
-	// Suppression des derniers espaces inutiles
-	$buffer = str_replace(array(' { ',' {','{ '), '{', $buffer);
-	$buffer = str_replace(array(' } ',' }','} '), '}', $buffer);
-	$buffer = str_replace(array(' : ',' :',': '), ':', $buffer);
-	$buffer = str_replace(array(';;',' ; ',' ;','; '), ';', $buffer);
-	$buffer = str_replace(array(' , ',' ,',', '), ',', $buffer);
-	$buffer = str_replace(':0px;', ':0;', $buffer);
+    // Suppression des tabulations, espaces multiples, retours à la ligne, etc.
+    $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '	 ', '	 '), '', $buffer);
 
-	// == Mise en page améliorée == 
-	
-	// Début du listing des propriétés
-	$buffer = str_replace('{', ' {'."\n", $buffer);	
-	$buffer = str_replace(';', ';'."\n", $buffer);	
-	$buffer = str_replace("\n\n","\n",$buffer);
-	
-	// Fin du listing des propriétés
-	$buffer = str_replace('}', "\n".'}'."\n", $buffer);
-	return $buffer;
+    // Suppression des derniers espaces inutiles
+    $buffer = str_replace(array(' { ', ' {', '{ '), '{', $buffer);
+    $buffer = str_replace(array(' } ', ' }', '} '), '}', $buffer);
+    $buffer = str_replace(array(' : ', ' :', ': '), ':', $buffer);
+    $buffer = str_replace(array(';;', ' ; ', ' ;', '; '), ';', $buffer);
+    $buffer = str_replace(array(' , ', ' ,', ', '), ',', $buffer);
+    $buffer = str_replace(':0px;', ':0;', $buffer);
+
+    // == Mise en page améliorée ==
+    // Début du listing des propriétés
+    $buffer = str_replace('{', ' {' . "\n", $buffer);
+    $buffer = str_replace(';', ';' . "\n", $buffer);
+    $buffer = str_replace("\n\n", "\n", $buffer);
+
+    // Fin du listing des propriétés
+    $buffer = str_replace('}', "\n" . '}' . "\n", $buffer);
+    return $buffer;
 }
 
 // Tri des propriétés
-function sort_css($buffer,$indentation = '   '){
-	global $listing_proprietes;
-	$buffer = str_replace("\n\n","\n",$buffer);
-	
-	$lignes_css = explode("\n",$buffer);
-	$lignes_retour_css = array();
-	foreach ($lignes_css as &$selecteur) {
-		if(strpos($selecteur,';') !== FALSE){
-			$proprietes = explode(';',$selecteur);
-			$proprietes_tmp = array();
-			foreach($proprietes as &$ligne_propriete){
-				$valeurs = explode(':',$ligne_propriete);
-				if(isset($valeurs[1])){
-					$proprietes_tmp[$valeurs[0]] = $valeurs[1];
-				}
-			}
-			$proprietes_retour = array();
-			foreach($listing_proprietes as $propriete){
-				if(isset($proprietes_tmp[$propriete])){
-					$proprietes_retour[] = $indentation.$propriete.':'.$proprietes_tmp[$propriete].';';
-					unset($proprietes_tmp[$propriete]);
-				}
-			}
-			
-			$proprietes_fin = array();
-			foreach($proprietes_tmp as $prop => $value)
-				$proprietes_fin[] = $indentation.$prop.':'.$value.';';
-			
-			$lignes_retour_css[] = implode("\n",$proprietes_retour).(!empty($proprietes_fin) ? "\n".implode("\n",$proprietes_fin):'');
-		}
-		else $lignes_retour_css[] = $selecteur;
-	}
-	$buffer = implode("\n",$lignes_retour_css);
-	return $buffer;
+function sort_css($buffer, $indentation = '   ', $separateur = ':') {
+    global $listing_proprietes;
+    $buffer = str_replace("\n\n", "\n", $buffer);
+
+    $lignes_css = explode("\n", $buffer);
+    $lignes_retour_css = array();
+    foreach ($lignes_css as &$selecteur) {
+        if (strpos($selecteur, ';') !== FALSE) {
+            $proprietes = explode(';', $selecteur);
+            $proprietes_tmp = array();
+            foreach ($proprietes as &$ligne_propriete) {
+                $valeurs = explode(':', $ligne_propriete);
+                if (isset($valeurs[1])) {
+                    $proprietes_tmp[$valeurs[0]] = $valeurs[1];
+                }
+            }
+            $proprietes_retour = array();
+            foreach ($listing_proprietes as $propriete) {
+                if (isset($proprietes_tmp[$propriete])) {
+                    $proprietes_retour[] = $indentation . $propriete . $separateur . $proprietes_tmp[$propriete] . ';';
+                    unset($proprietes_tmp[$propriete]);
+                }
+            }
+
+            $proprietes_fin = array();
+            foreach ($proprietes_tmp as $prop => $value)
+                $proprietes_fin[] = $indentation . $prop . $separateur . $value . ';';
+
+            $lignes_retour_css[] = implode("\n", $proprietes_retour) . (!empty($proprietes_fin) ? "\n" . implode("\n", $proprietes_fin) : '');
+        }
+        else
+            $lignes_retour_css[] = $selecteur;
+    }
+    $buffer = implode("\n", $lignes_retour_css);
+    return $buffer;
 }
--- inc/header.php
+++ inc/header.php
@@ -1,24 +1,22 @@
 <?php
-include dirname(__FILE__).'/config.php';
-include dirname(__FILE__).'/valeurs.php';
-include dirname(__FILE__).'/functions.php';
 
-if(isset($_POST['clean_css'])){
-	$indentation = '    ';
-	
-	$buffer = clean_css(strip_tags($_POST['clean_css']));
-	
-	$buffer = sort_css($buffer,$indentation);
-	
-	
-	// Suppression des doubles lignes
-	$buffer = str_replace("\n\n","\n",$buffer);
-		
-	if(isset($_POST['type_separateur']) && array_key_exists($_POST['type_separateur'],$listing_separateurs))
-		$separateur = $_POST['type_separateur'];
-		
-	// Espacement entre propriété et valeur
-	$buffer = str_replace(':', $listing_separateurs[$separateur], $buffer);
-	
-	$content = $buffer;
+include dirname(__FILE__) . '/config.php';
+include dirname(__FILE__) . '/valeurs.php';
+include dirname(__FILE__) . '/functions.php';
+
+if (isset($_POST['clean_css'])) {
+    $indentation = '    ';
+
+    $buffer = clean_css(strip_tags($_POST['clean_css']));
+
+    $separateur = 0;
+    if (isset($_POST['type_separateur']) && array_key_exists($_POST['type_separateur'], $listing_separateurs))
+        $separateur = $_POST['type_separateur'];
+
+    $buffer = sort_css($buffer, $indentation, $listing_separateurs[$separateur]);
+
+    // Suppression des doubles lignes
+    $buffer = str_replace("\n\n", "\n", $buffer);
+
+    $content = $buffer;
 }
