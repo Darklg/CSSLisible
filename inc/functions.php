c8af917ae6276790f2c1a851e2a7208672e87bee
Kevin <kevin@colorz.fr>
2011-09-15 18:14:04 +0200

    Refonte de l'algorithme de tri des proprietes et liste de bugs

--- inc/functions.php
+++ inc/functions.php
@@ -29,37 +29,73 @@ function clean_css($buffer) {
 // Tri des propriétés
 function sort_css($buffer, $indentation = '   ', $separateur = ':') {
     global $listing_proprietes;
-    $buffer = str_replace("\n\n", "\n", $buffer);
 
-    $lignes_css = explode("\n", $buffer);
-    $lignes_retour_css = array();
-    foreach ($lignes_css as &$selecteur) {
-        if (strpos($selecteur, ';') !== FALSE) {
-            $proprietes = explode(';', $selecteur);
-            $proprietes_tmp = array();
-            foreach ($proprietes as &$ligne_propriete) {
-                $valeurs = explode(':', $ligne_propriete);
-                if (isset($valeurs[1])) {
-                    $proprietes_tmp[$valeurs[0]] = $valeurs[1];
+    $buffer_props = explode('}', $buffer);
+    $new_props = array();
+    // On divise par propriétés
+    foreach ($buffer_props as $prop) {
+        $lines = explode("\n", $prop);
+        $new_lines = array();
+        $properties_tmp = array();
+        $properties_dbl = array();
+        // On divise par ligne
+
+        foreach ($lines as $line) {
+            $line_t = trim($line);
+            $values = explode(':', $line_t);
+            // C'est un selecteur, on l'ajoute à la suite.
+            if (!isset($values[1]) || strpos($line_t,'{') !== FALSE) {
+                if (!empty($line_t)) {
+                    $new_lines[] = $line_t;
+                }
+            } else {
+                // On supprime les ; de fin de ligne
+                if (substr($values[1], -1) == ';')
+                    $values[1] = substr($values[1], 0, -1);
+                // On met de côté la propriété
+                if (!isset($properties_tmp[$values[0]])) {
+                    $properties_tmp[$values[0]] = $values[1];
+                } else {
+                    if (!isset($properties_dbl[$values[0]]))
+                        $properties_dbl[$values[0]] = array();
+                    $properties_dbl[$values[0]][] = $values;
                 }
             }
-            $proprietes_retour = array();
-            foreach ($listing_proprietes as $propriete) {
-                if (isset($proprietes_tmp[$propriete])) {
-                    $proprietes_retour[] = $indentation . $propriete . $separateur . $proprietes_tmp[$propriete] . ';';
-                    unset($proprietes_tmp[$propriete]);
-                }
-            }
-
-            $proprietes_fin = array();
-            foreach ($proprietes_tmp as $prop => $value)
-                $proprietes_fin[] = $indentation . $prop . $separateur . $value . ';';
-
-            $lignes_retour_css[] = implode("\n", $proprietes_retour) . (!empty($proprietes_fin) ? "\n" . implode("\n", $proprietes_fin) : '');
         }
-        else
-            $lignes_retour_css[] = $selecteur;
+
+        // On trie les proprietes récupérées
+        foreach ($listing_proprietes as $propriete) {
+            if (isset($properties_tmp[$propriete])) {
+                $new_lines[] = $indentation . $propriete . $separateur . $properties_tmp[$propriete] . ';';
+                unset($properties_tmp[$propriete]);
+            }
+            // On regarde aussi dans les doublons
+            if (isset($properties_dbl[$propriete])) {
+                foreach ($properties_dbl[$propriete] as $values) {
+                    $new_lines[] = $indentation . $values[0] . $separateur . $values[1] . ';';
+                }
+                unset($properties_dbl[$propriete]);
+            }
+        }
+
+		// On ajoute les proprietes qui n'ont pas été affichée pour l'instant
+        foreach ($properties_tmp as $propriete => $valeur) {
+            $new_lines[] = $indentation . $propriete . $separateur . $valeur . ';';
+			// On regarde aussi dans les doublons
+            if (isset($properties_dbl[$propriete])) {
+                foreach ($properties_dbl[$propriete] as $values) {
+                    $new_lines[] = $indentation . $values[0] . $separateur . $values[1] . ';';
+                }
+                unset($properties_dbl[$propriete]);
+            }
+        }
+
+
+
+        $new_props[] = implode("\n", $new_lines);
     }
-    $buffer = implode("\n", $lignes_retour_css);
+
+    $buffer = implode("\n" . '}' . "\n", $new_props);
     return $buffer;
 }
+
