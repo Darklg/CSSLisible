04296474e001531337f9a3f2b65f6911733418b9
Darklg <kev.rocher@gmail.com>
2011-12-14 14:41:01 +0100

    Ordre des valeurs + nouvelles valeurs

--- inc/valeurs.php
+++ inc/valeurs.php
@@ -1,21 +1,29 @@
 <?php
 $listing_proprietes = array(
-
-	'z-index',
 	
+	'z-index',
+		
+	'display',
+	'visibility',
+
+	'float',
+	'clear',
+
 	'position',
 	
-	'display',
-	
-	'float',
-	
-	'width',   		
-	'height',
-	
+	'zoom',
+
 	'top',
 	'right',
 	'bottom',
 	'left',
+	
+/* --------------------------------------------------------------------
+	Box Model
+   ----------------------------------------------------------------- */
+	
+	'width',   		
+	'height',
 	
 	'margin',
 	'margin-bottom',
@@ -28,47 +36,89 @@ $listing_proprietes = array(
 	'padding-left',
 	'padding-right',
 	'padding-top',
-	
-	'overflow',
-	
-	'azimuth',
-	'caption-side',
-	'clear',
-	'clip',
-	'content',
-	'counter-increment',
-	'counter-reset',
-	'cursor',
-	'direction',
-	'empty-cells',
-	'letter-spacing',
-	'list-style',
-	'list-style-image',
-	'list-style-position',
-	'list-style-type',
 
 	'max-height',
 	'max-width',
 	'min-height',
 	'min-width',
-	'orphans',
-	'page-break-after',
-	'page-break-before',
-	'page-break-inside',
-	'quotes',
+	
+	'border',
+
+	'border-bottom',
+	'border-bottom-color',
+	'border-bottom-style',
+	'border-bottom-width',
+
+	'border-left',
+	'border-left-color',
+	'border-left-style',
+	'border-left-width',
+	
+	'border-right',
+	'border-right-color',
+	'border-right-style',
+	'border-right-width',
+	
+	'border-top',
+	'border-top-color',
+	'border-top-style',
+	'border-top-width',
+	
+	'border-collapse',
+	'border-color',
+	'border-spacing',
+	'border-style',
+	'border-width',
+
+	'-moz-border-radius',
+	'-webkit-border-radius',
+	'-khtml-border-radius',
+	'border-radius',
+	
+	'-webkit-box-align',
+	'-moz-box-align',
+	'-ms-box-align',
+	'box-align',
+	
+	'-webkit-box-pack',
+	'-moz-box-pack',
+	'-ms-box-pack',
+	'box-pack',
+	
+	'-o-box-sizing',
+	'-webkit-box-sizing',
+	'-moz-box-sizing',
+	'box-sizing',
+	
+/* --------------------------------------------------------------------
+	Proprietes de texte
+   ----------------------------------------------------------------- */
+	
+	'overflow',
+	'overflow-x',
+	'overflow-y',
+	
+	'azimuth',
+	'caption-side',
+	'clip',
+	'content',
+	'counter-increment',
+	'counter-reset',
+	'direction',
+	'empty-cells',
+	'list-style',
+	'list-style-image',
+	'list-style-position',
+	'list-style-type',
+	
 	'table-layout',
+	
 	'text-align',
 	'text-decoration',
 	'text-indent',
 	'text-shadow',
 	'text-transform',
-	'unicode-bidi',
-	'vertical-align',
-	'visibility',
-	'white-space',
-	'widows',
-	'word-spacing',
-	
+
 	'font',
 	'font-family',
 	'font-size',
@@ -76,45 +126,33 @@ $listing_proprietes = array(
 	'font-variant',
 	'font-weight',
 	'line-height',
-
-	'border',
-	'border-bottom',
-	'border-bottom-color',
-	'border-bottom-style',
-	'border-bottom-width',
-	'border-collapse',
-	'border-color',
-	'border-left',
-	'border-left-color',
-	'border-left-style',
-	'border-left-width',
-	'border-right',
-	'border-right-color',
-	'border-right-style',
-	'border-right-width',
-	'border-spacing',
-	'border-style',
 	
-	'border-top',
-	'border-top-color',
-	'border-top-style',
-	'border-top-width',
-	'border-width',
+	'letter-spacing',
+	'white-space',
+	'word-spacing',
 	
-	'-moz-border-radius',
-	'-webkit-border-radius',
-	'-khtml-border-radius',
-	'border-radius',
+	'orphans',
+	'page-break-after',
+	'page-break-before',
+	'page-break-inside',
+	'quotes',
+	'unicode-bidi',
+	'vertical-align',
+	'widows',
 	
 	'outline',
 	'outline-color',
 	'outline-style',
 	'outline-width',
 	
+/* --------------------------------------------------------------------
+	Couleur & Background
+   ----------------------------------------------------------------- */
+	
 	'color',
 	
 	'background',
-	'background-attachment',  		
+	'background-attachment',
 	'background-color',
 	'background-image',
 	'background-position',
@@ -138,8 +176,21 @@ $listing_proprietes = array(
 	
 	'-webkit-transition',
 	'-moz-transition',
+	'-ms-transition',
 	'-o-transition',
 	'transition',
+	
+	'-webkit-animation',
+	'-moz-animation',
+	'-o-animation',
+	'animation',
+	
+/* --------------------------------------------------------------------
+	Comportements
+   ----------------------------------------------------------------- */
+	
+	'resize',
+	'cursor',
 	
 	'-moz-user-select',
 	'-webkit-user-select',
@@ -148,19 +199,5 @@ $listing_proprietes = array(
 	'-moz-user-drag',
 	'-webkit-user-drag',
 	'user-drag',
-	
-	'-moz-box-align',
-	'-webkit-box-align',
-	'-ms-box-align',
-	'box-align',
-	
-	'-moz-box-pack',
-	'-webkit-box-pack',
-	'-ms-box-pack',
-	'box-pack',
-	
-	'-moz-box-sizing',
-	'-webkit-box-sizing',
-	' box-sizing',
 
 );
