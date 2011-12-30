fadbcd873305b25054ff6f2b41e358f2465c3472
Darklg <kev.rocher@gmail.com>
2011-12-30 16:57:37 +0100

    toggle pour les options
    Merci @NumEricR :
    https://github.com/NumEricR/CSSLisible/commit/4d8181bb3109cefe96d5a4a6ac
    694a547a513ff5

--- js/events.js
+++ js/events.js
@@ -5,9 +5,13 @@ function $(id){
 if($('options_toggle') && $('options_block')){
 	// On masque les options
 	$('options_block').style.display = 'none';
-	// On ne les affiche qu'au clic du bouton d'options
+	// On ne les affiche/masque qu'au clic du bouton d'options
 	$('options_toggle').onclick = function(){
-		$('options_block').style.display = 'block';
+		toggle($('options_block'));
 		return false;
 	}
+}
+
+function toggle(element){
+	return (element.style.display = (element.style.display == 'block') ? 'none' : 'block');
 }
