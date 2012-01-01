a00d07893ba2cf7f0397a8ff4e4b6375ddd364fd
Darklg <kev.rocher@gmail.com>
2012-01-01 17:32:09 +0100

    Fix aff. options & Reindent events

--- js/events.js
+++ js/events.js
@@ -12,14 +12,11 @@ function show_hide_blocks_if_checked(element_checked,blocks){
 	}
 }
 
-
-
+// Affiche ou non des blocs selon le type d'indentation demandé.
 if($('selecteur_par_ligne') && $('tout_compresse')){
 	
 	var blocks_options_selecteur = ['block_tout_compresse','block_distance_selecteurs','block_selecteurs_multiples_separes'];
-
 	var blocks_options_tout = ['block_selecteur_par_ligne','block_type_separateur','block_distance_selecteurs','block_type_indentation','block_selecteurs_multiples_separes'];
-	
 	
 	if($('selecteur_par_ligne').checked){
 		show_hide_blocks_if_checked($('selecteur_par_ligne'),blocks_options_selecteur);
@@ -36,12 +33,9 @@ if($('selecteur_par_ligne') && $('tout_compresse')){
 	}
 }
 
-
-
-
 if($('options_toggle') && $('options_block')){
 	// On masque les options
-	//$('options_block').style.display = 'none';
+	$('options_block').style.display = 'none';
 	// On ne les affiche/masque qu'au clic du bouton d'options
 	$('options_toggle').onclick = function(){
 		toggle($('options_block'));
