a24385f50fb01f76a1998e12fb31f8333bc997d3
Darklg <kev.rocher@gmail.com>
2012-02-20 23:51:03 +0100

    Début retab project

--- inc/tpl/formulaire/type_separateur.php
+++ inc/tpl/formulaire/type_separateur.php
@@ -1,10 +1,10 @@
 <div class="select-block" id="block_type_separateur">
 	<span class="in-block">
-        <select name="type_separateur" id="type_separateur">
-        <?php foreach ($CSSLisible->listing_separateurs as $key => $this_separateur) : ?>
-            <option value="<?php echo $key; ?>" <?php echo ($this_separateur == $CSSLisible->get_option('separateur') ? 'selected="selected"' : ''); ?>>&quot;<?php echo $this_separateur; ?>&quot;</option>
-        <?php endforeach; ?>
-        </select>
+		<select name="type_separateur" id="type_separateur">
+		<?php foreach ($CSSLisible->listing_separateurs as $key => $this_separateur) : ?>
+			<option value="<?php echo $key; ?>" <?php echo ($this_separateur == $CSSLisible->get_option('separateur') ? 'selected="selected"' : ''); ?>>&quot;<?php echo $this_separateur; ?>&quot;</option>
+		<?php endforeach; ?>
+		</select>
 	</span>
    <label for="type_separateur">Type de s&eacute;parateur</label>
 </div>
--- index.php
+++ index.php
@@ -3,25 +3,25 @@ include dirname(__FILE__) . '/inc/header.php';
 ?>
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8"/>
-        <title><?php echo TITRE_SITE; ?></title>
+	<head>
+		<meta charset="utf-8"/>
+		<title><?php echo TITRE_SITE; ?></title>
 		<link rel="stylesheet" href="css/main.css?201112141443" type="text/css" />
-    </head>
-    <body>
+	</head>
+	<body>
 	<div id="main-container">
-        <h1><?php echo TITRE_SITE; ?></h1>
-        <form id="main-form" action="" method="post">
+		<h1><?php echo TITRE_SITE; ?></h1>
+		<form id="main-form" action="" method="post">
 			<p>
 				CSSLisible va r&eacute;indenter vos blocks de code, 
 				<a href="http://blog.goetter.fr/post/14503308074/ordonnez-vos-declarations-css" rel="external">ordonner vos propri&eacute;t&eacute;s</a>, 
 				afin de fournir un code CSS Lisible et maintenable. 
 				Attention, les commentaires internes aux sélecteurs sont retir&eacute;s !
 			</p>
-            <div class="form-block">
-                <label for="clean_css">CSS &agrave; nettoyer :</label><br />
-                <textarea name="clean_css" id="clean_css" rows="12" cols="80"><?php echo $CSSLisible->buffer; ?></textarea>
-            </div>
+			<div class="form-block">
+				<label for="clean_css">CSS &agrave; nettoyer :</label><br />
+				<textarea name="clean_css" id="clean_css" rows="12" cols="80"><?php echo $CSSLisible->buffer; ?></textarea>
+			</div>
 			<div id="options_block">
 				<div class="the_grid gri-2-1">
 					<div>
@@ -41,14 +41,14 @@ include dirname(__FILE__) . '/inc/header.php';
 				</div>
 			</div>
 			<div class="submit-block">
-            	<button class="go_clean_css">Nettoyer le code &rarr;</button>
-            	<button id="options_toggle" class="go_clean_css go_options">&rarrhk; Options</button>
+				<button class="go_clean_css">Nettoyer le code &rarr;</button>
+				<button id="options_toggle" class="go_clean_css go_options">&rarrhk; Options</button>
 			</div>
-        </form>
+		</form>
 	</div>
 	<a href="http://github.com/darklg/CSSLisible"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" /></a>
 	
 	<script src="js/events.js?1329773844" type="text/javascript" charset="utf-8"></script>
 	
-    </body>
+	</body>
 </html>
