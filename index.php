8eafedc0cfad2eff13de68fe9cf8c46bfb39f130
NumEricR <eric.github@gmail.com>
2012-05-11 10:14:33 +0200

    Ajout d'un paramètre pour changer le format des couleurs

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ include dirname(__FILE__) . '/inc/header.php';
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/distance_selecteurs.php'; ?>
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/type_indentation.php'; ?>
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/type_separateur.php'; ?>
+						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/colors_format.php'; ?>
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/hex_colors_format.php'; ?>
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/selecteurs_multiples_separes.php'; ?>
 						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/supprimer_selecteurs_vides.php'; ?>
