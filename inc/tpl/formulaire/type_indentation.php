1d4b9ab745dac73151d6c85c599a4fef6188565a
Darklg <kev.rocher@gmail.com>
2012-07-11 22:45:26 +0200

    Unification des noms des options et du POST - Vérifications de sécurité lors de l'acquisition des options

--- inc/tpl/formulaire/type_indentation.php
+++ inc/tpl/formulaire/type_indentation.php
@@ -2,7 +2,7 @@
 	<span class="in-block">
 		<select name="type_indentation" id="type_indentation">
 		<?php foreach($CSSLisible->listing_indentations as $key => $indentation) : ?>
-			<option value="<?php echo $key; ?>" <?php echo ($key == $CSSLisible->get_option('indentation') ? 'selected="selected"' : ''); ?>><?php echo $indentation[1]; ?></option>
+			<option value="<?php echo $key; ?>" <?php echo ($key == $CSSLisible->get_option('type_indentation') ? 'selected="selected"' : ''); ?>><?php echo $indentation[1]; ?></option>
 		<?php endforeach; ?>
 		</select>
 	</span>
--- inc/tpl/formulaire/type_separateur.php
+++ inc/tpl/formulaire/type_separateur.php
@@ -2,7 +2,7 @@
 	<span class="in-block">
 		<select name="type_separateur" id="type_separateur">
 		<?php foreach ($CSSLisible->listing_separateurs as $key => $this_separateur) : ?>
-			<option value="<?php echo $key; ?>" <?php echo ($key == $CSSLisible->get_option('separateur') ? 'selected="selected"' : ''); ?>>&quot;<?php echo $this_separateur; ?>&quot;</option>
+			<option value="<?php echo $key; ?>" <?php echo ($key == $CSSLisible->get_option('type_separateur') ? 'selected="selected"' : ''); ?>>&quot;<?php echo $this_separateur; ?>&quot;</option>
 		<?php endforeach; ?>
 		</select>
 	</span>
