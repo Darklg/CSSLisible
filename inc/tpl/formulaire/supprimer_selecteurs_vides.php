1d676ae03c062faef15ef7954cc38ecf6a93e210
Darklg <kev.rocher@gmail.com>
2012-07-31 22:20:46 +0200

    Refonte de la présentation sur 3 colonnes d'options

--- inc/tpl/formulaire/supprimer_selecteurs_vides.php
+++ inc/tpl/formulaire/supprimer_selecteurs_vides.php
@@ -1,6 +1,4 @@
 <div class="check-block" id="block_supprimer_selecteurs_vides">
-	<span class="in-block">
-		<input type="checkbox" name="supprimer_selecteurs_vides" id="supprimer_selecteurs_vides" <?php echo ($CSSLisible->get_option('supprimer_selecteurs_vides') ? 'checked="checked"':''); ?>/>
-	</span>
+	<input type="checkbox" name="supprimer_selecteurs_vides" id="supprimer_selecteurs_vides" <?php echo ($CSSLisible->get_option('supprimer_selecteurs_vides') ? 'checked="checked"':''); ?>/>
 	<label for="supprimer_selecteurs_vides">Supprimer les s&eacute;lecteurs vides</label>
 </div>
