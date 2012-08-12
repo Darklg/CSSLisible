ec291d60a9be778b162681124755c97fe21d60c8
Darklg <kev.rocher@gmail.com>
2012-08-12 23:36:00 +0200

    Menu de traduction

--- inc/traduction.php
+++ inc/traduction.php
@@ -2,12 +2,21 @@
 
 $lang = 'fr_FR';
 $more_languages = array(
-    'en' => 'en_US.utf8',
+    'fr' =>array(
+        'lang' => 'fr_FR',
+        'name' => 'Français',
+        'active' => 0
+    ),
+    'en' => array(
+        'lang' => 'en_US.utf8',
+        'name' => 'English',
+        'active' => 1
+    ),
 );
 
 // Si la traduction en la langue demandée est disponible
-if(isset($_GET['lang']) && array_key_exists($_GET['lang'],$more_languages)){
-    $lang = $more_languages[$_GET['lang']];
+if(isset($_GET['lang']) && array_key_exists($_GET['lang'],$more_languages) && $more_languages[$_GET['lang']]['active']){
+    $lang = $more_languages[$_GET['lang']]['lang'];
 }
 
 // On définit la langue
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-include dirname(__FILE__) . '/inc/header.php';
+define('DIR_SITE', DIR_SITE);
+include DIR_SITE . '/inc/header.php';
 ?>
 <!doctype html>
 <html lang="<?php echo $lang; ?>">
@@ -7,49 +8,47 @@ include dirname(__FILE__) . '/inc/header.php';
 		<meta charset="utf-8"/>
 		<title><?php echo TITRE_SITE . ' - ' . SLOGAN_SITE; ?></title>
 		<meta name="viewport" content="width=790" />
-		<link rel="stylesheet" href="css/main.css?t=201208122256" type="text/css" />
+		<link rel="stylesheet" href="css/main.css?t=201208122335" type="text/css" />
 	</head>
 	<body class="no-js">
 	<div id="main-container">
-		<h1><?php echo TITRE_SITE . ' - ' . SLOGAN_SITE; ?></h1>
+		<div id="header">
+		    <?php include DIR_SITE . '/inc/tpl/lang_menu.php'; ?>
+    		<h1><?php echo TITRE_SITE . ' - ' . SLOGAN_SITE; ?></h1>
+		</div>
 		<form id="main-form" action="" method="post" enctype="multipart/form-data">
-			<p>
-				<?php echo _('CSSLisible va réindenter vos blocks de code,'); ?> 
-				<a lang="fr" href="http://blog.goetter.fr/post/14503308074/ordonnez-vos-declarations-css" rel="external" target="_blank"><?php echo _('ordonner vos propriétés'); ?></a>, 
-				<?php echo _('afin de fournir un code CSS Lisible et plus maintenable. '); ?>
-				<?php echo _('Attention, les commentaires internes aux sélecteurs sont retirés !'); ?>
-			</p>
 
-			<?php include dirname(__FILE__) . '/inc/tpl/errors.php'; ?>
-			<?php include dirname(__FILE__) . '/inc/tpl/tabs.php'; ?>
+			<?php include DIR_SITE . '/inc/tpl/text.php'; ?>
+			<?php include DIR_SITE . '/inc/tpl/errors.php'; ?>
+			<?php include DIR_SITE . '/inc/tpl/tabs.php'; ?>
 
 			<div id="options_block">
 				<div class="the_grid gri-4-3-3">
 					<fieldset>
 					    <legend id="titre-formatage"><?php echo _('Formatage'); ?></legend>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/distance_selecteurs.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/type_indentation.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/type_separateur.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/colors_format.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/hex_colors_format.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/distance_selecteurs.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/type_indentation.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/type_separateur.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/colors_format.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/hex_colors_format.php'; ?>
 						&nbsp;</fieldset>
 					<fieldset>
 					    <legend id="titre-presentation"><?php echo _('Présentation'); ?></legend>
-					    <?php include dirname(__FILE__) . '/inc/tpl/formulaire/selecteurs_multiples_separes.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/valeurs_multiples_separees.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/supprimer_selecteurs_vides.php'; ?>
+					    <?php include DIR_SITE . '/inc/tpl/formulaire/selecteurs_multiples_separes.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/valeurs_multiples_separees.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/supprimer_selecteurs_vides.php'; ?>
                        
 					&nbsp;</fieldset>
 					<fieldset>
 					    <legend><?php echo _('Options avancées'); ?></legend>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/tout_compresse.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/selecteur_par_ligne.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/return_file.php'; ?>
-						<?php include dirname(__FILE__) . '/inc/tpl/formulaire/add_header.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/tout_compresse.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/selecteur_par_ligne.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/return_file.php'; ?>
+						<?php include DIR_SITE . '/inc/tpl/formulaire/add_header.php'; ?>
 					&nbsp;</fieldset>
 				</div>
 			</div>
-			<?php include dirname(__FILE__) . '/inc/tpl/submit.php'; ?>
+			<?php include DIR_SITE . '/inc/tpl/submit.php'; ?>
 		</form>
 	</div>
 	<div id="footer">
@@ -60,18 +59,17 @@ include dirname(__FILE__) . '/inc/header.php';
 		<a target="_blank" href="http://github.com/Darklg">Darklg</a>,
 		<a target="_blank" href="http://github.com/NumEricR">NumEricR</a>
 	</div>
-	<a href="http://github.com/darklg/CSSLisible"><img style="position: absolute; top: 0; right: 0; border: 0;" src="img/fork-me.png" alt="Fork me on GitHub" /></a>
 	
 	<?php
 	// Fichier conditionnel pour charger boutons like, google analytics, etc.
-	$hollow_file = realpath(dirname(__FILE__)).'/inc/hollow-file.php';
+	$hollow_file = realpath(DIR_SITE).'/inc/hollow-file.php';
 	if(file_exists($hollow_file)){
 	    include $hollow_file;
 	}
 	?>
 	
 	
-	<script src="js/events.js?t=201208122256" type="text/javascript" charset="utf-8"></script>
+	<script src="js/events.js?t=201208122335" type="text/javascript" charset="utf-8"></script>
 	
 	</body>
 </html>
