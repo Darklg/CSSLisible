03aa7e0db8b5e59512b97bdb064a141638472757
Darklg <kev.rocher@gmail.com>
2012-08-18 15:10:03 +0200

    fixes #34 Ajout de l'encodage dans le retour API

--- inc/header.php
+++ inc/header.php
@@ -8,6 +8,6 @@ include dirname(__FILE__) . '/functions.php';
 $CSSLisible = new CSSLisible($listing_proprietes);
 
 if(isset($_POST['api']) || isset($_GET['api'])) {
+    header('Content-type: text/css; charset=utf-8');
 	exit($CSSLisible->buffer);
 }
-
