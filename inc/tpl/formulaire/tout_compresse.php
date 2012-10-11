19890128118d11bbce36b5a80fceecfb193c7f8d
Darklg <kev.rocher@gmail.com>
2012-10-11 15:19:54 +0200

    Correction API pour booleens

--- inc/tpl/formulaire/tout_compresse.php
+++ inc/tpl/formulaire/tout_compresse.php
@@ -1,4 +1,4 @@
 <div class="check-block" id="block_tout_compresse">
-	<input type="checkbox" name="tout_compresse" id="tout_compresse" <?php echo ($CSSLisible->get_option('tout_compresse') ? 'checked="checked"':''); ?>/>
+	<input type="checkbox" value="1" name="tout_compresse" id="tout_compresse" <?php echo ($CSSLisible->get_option('tout_compresse') ? 'checked="checked"':''); ?>/>
 	<label for="tout_compresse"><?php echo _('Tout compressé !'); ?></label>
 </div>
