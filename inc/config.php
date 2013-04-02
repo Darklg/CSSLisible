ce8cf3206b91d2b39a20384f5fdc2055ffa7e7e7
Darklg <kev.rocher@gmail.com>
2013-04-02 22:19:32 +0200

    Gestion d'une configuration par défaut par utilisateur

--- inc/config.php
+++ inc/config.php
@@ -1,12 +1,19 @@
 <?php
-define('TITRE_SITE', 'CSSLisible');
-define('SLOGAN_SITE', _('Ordonner votre CSS et le rendre lisible'));
+if ( !defined( 'TITRE_SITE' ) )
+    define( 'TITRE_SITE', 'CSSLisible' );
+if ( !defined( 'SLOGAN_SITE' ) )
+    define( 'SLOGAN_SITE', _( 'Ordonner votre CSS et le rendre lisible' ) );
+if ( !defined( 'COOKIE_NAME' ) )
+    define( 'COOKIE_NAME', 'CSSLisible' );
+if ( !defined( 'CURLOPT_USERAGENT_NAME' ) )
+    define( 'CURLOPT_USERAGENT_NAME', 'CSSLisible' );
 
-define('COOKIE_NAME', 'CSSLisible');
-define('CURLOPT_USERAGENT_NAME', 'CSSLisible');
+/* Limites */
+if ( !defined( 'MAX_FILESIZE' ) )
+    define( 'MAX_FILESIZE', 307200 );
 
-define('MAX_FILESIZE', 307200);
-
-define('USE_TEST_BUTTON', true);
-
-define('USE_COPY_BUTTON', true);
+/* Fonctionnalités */
+if ( !defined( 'USE_TEST_BUTTON' ) )
+    define( 'USE_TEST_BUTTON', true );
+if ( !defined( 'USE_COPY_BUTTON' ) )
+    define( 'USE_COPY_BUTTON', true );
--- inc/header.php
+++ inc/header.php
@@ -1,13 +1,22 @@
 <?php
 
-include dirname(__FILE__) . '/traduction.php';
-include dirname(__FILE__) . '/config.php';
-include dirname(__FILE__) . '/valeurs.php';
-include dirname(__FILE__) . '/functions.php';
+/* Configuration */
+$userConfig = dirname( __FILE__ ) . '/user-config.php';
+if ( file_exists( $userConfig ) ) {
+    include $userConfig;
+}
+include dirname( __FILE__ ) . '/config.php';
 
-$CSSLisible = new CSSLisible($listing_proprietes);
+/* Fonctions de traduction */
+include dirname( __FILE__ ) . '/traduction.php';
 
-if(isset($_POST['api']) || isset($_GET['api'])) {
-    header('Content-type: text/css; charset=utf-8');
-	exit($CSSLisible->buffer);
+/* Core */
+include dirname( __FILE__ ) . '/valeurs.php';
+include dirname( __FILE__ ) . '/functions.php';
+
+$CSSLisible = new CSSLisible( $listing_proprietes );
+
+if ( isset( $_POST['api'] ) || isset( $_GET['api'] ) ) {
+    header( 'Content-type: text/css; charset=utf-8' );
+    exit( $CSSLisible->buffer );
 }
