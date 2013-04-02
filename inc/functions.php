f09d5b8a90eb328b8e05547dddcfc64150941e49
NumEricR <eric.github@gmail.com>
2013-04-02 11:05:39 +0200

    Ajout d'un paramètre en config pour le nom du user agent utilisé via cURL

--- inc/functions.php
+++ inc/functions.php
@@ -203,7 +203,7 @@ class CSSLisible {
 	    $ch = curl_init();
         curl_setopt ($ch, CURLOPT_URL, $url);
         curl_setopt ($ch, CURLOPT_HEADER, 0);
-        curl_setopt ($ch, CURLOPT_USERAGENT, 'CSSLisible');
+        curl_setopt ($ch, CURLOPT_USERAGENT, CURLOPT_USERAGENT_NAME);
         curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_exec($ch);
