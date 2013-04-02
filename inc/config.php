f6f31318632575195b4cbd99cf0ed0db71d65ef7
NumEricR <eric.github@gmail.com>
2013-04-02 11:05:39 +0200

    Ajout d'un paramètre en config pour le nom du user agent utilisé via cURL

--- inc/config.php
+++ inc/config.php
@@ -3,6 +3,7 @@ define('TITRE_SITE', 'CSSLisible');
 define('SLOGAN_SITE', _('Ordonner votre CSS et le rendre lisible'));
 
 define('COOKIE_NAME', 'CSSLisible');
+define('CURLOPT_USERAGENT_NAME', 'CSSLisible');
 
 define('USE_TEST_BUTTON', true);
 
