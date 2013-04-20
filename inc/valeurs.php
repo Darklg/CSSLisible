626204500b24658fe783119e04b8e057b073f14e
Darklg <kev.rocher@gmail.com>
2013-04-20 12:09:36 +0200

    White space fix & retab on properties list.

--- inc/valeurs.php
+++ inc/valeurs.php
@@ -1,108 +1,108 @@
 <?php
 $listing_proprietes = array(
-	
-/* --------------------------------------------------------------------
-	Content
-   ----------------------------------------------------------------- */
-
-	'content',
-	'counter-increment',
-	'counter-reset',
-	'quotes',
 
 /* --------------------------------------------------------------------
-	Display
+    Content
    ----------------------------------------------------------------- */
 
-	'display',
-	
-	'visibility',
-	
-	'z-index',
-	
-	'float',
-	'clear',
-	
-	'position',
-	'top',
-	'right',
-	'bottom',
-	'left',
-	'zoom',
+    'content',
+    'counter-increment',
+    'counter-reset',
+    'quotes',
 
 /* --------------------------------------------------------------------
-	Box Model
+    Display
    ----------------------------------------------------------------- */
-	
-	'-webkit-box-align',
-	'-moz-box-align',
-	'-ms-box-align',
-	'box-align',
-	
-	'-webkit-box-pack',
-	'-moz-box-pack',
-	'-ms-box-pack',
-	'box-pack',
-	
-	'-webkit-box-sizing',
-	'-moz-box-sizing',
-	'-o-box-sizing',
-	'box-sizing',
 
-	'width',
-	'height',
-	
-	'margin',
-	'margin-top',
-	'margin-right',
-	'margin-bottom',
-	'margin-left',
-	
-	'padding',
-	'padding-top',
-	'padding-right',
-	'padding-bottom',
-	'padding-left',
+    'display',
 
-	'max-height',
-	'max-width',
-	'min-height',
-	'min-width',
-	
-	'border',
+    'visibility',
 
-	'border-top',
-	'border-top-color',
-	'border-top-style',
-	'border-top-width',
+    'z-index',
 
-	'border-right',
-	'border-right-color',
-	'border-right-style',
-	'border-right-width',
+    'float',
+    'clear',
 
-	'border-bottom',
-	'border-bottom-color',
-	'border-bottom-style',
-	'border-bottom-width',
+    'position',
+    'top',
+    'right',
+    'bottom',
+    'left',
+    'zoom',
 
-	'border-left',
-	'border-left-color',
-	'border-left-style',
-	'border-left-width',
+/* --------------------------------------------------------------------
+    Box Model
+   ----------------------------------------------------------------- */
 
-	'border-collapse',
-	'border-color',
-	'border-spacing',
-	'border-style',
-	'border-width',
+    '-webkit-box-align',
+    '-moz-box-align',
+    '-ms-box-align',
+    'box-align',
 
-	'-webkit-border-radius',
-	'-moz-border-radius',
-	'-khtml-border-radius',
-	'border-radius',
-	
-	'-webkit-border-top-left-radius',
+    '-webkit-box-pack',
+    '-moz-box-pack',
+    '-ms-box-pack',
+    'box-pack',
+
+    '-webkit-box-sizing',
+    '-moz-box-sizing',
+    '-o-box-sizing',
+    'box-sizing',
+
+    'width',
+    'height',
+
+    'margin',
+    'margin-top',
+    'margin-right',
+    'margin-bottom',
+    'margin-left',
+
+    'padding',
+    'padding-top',
+    'padding-right',
+    'padding-bottom',
+    'padding-left',
+
+    'max-height',
+    'max-width',
+    'min-height',
+    'min-width',
+
+    'border',
+
+    'border-top',
+    'border-top-color',
+    'border-top-style',
+    'border-top-width',
+
+    'border-right',
+    'border-right-color',
+    'border-right-style',
+    'border-right-width',
+
+    'border-bottom',
+    'border-bottom-color',
+    'border-bottom-style',
+    'border-bottom-width',
+
+    'border-left',
+    'border-left-color',
+    'border-left-style',
+    'border-left-width',
+
+    'border-collapse',
+    'border-color',
+    'border-spacing',
+    'border-style',
+    'border-width',
+
+    '-webkit-border-radius',
+    '-moz-border-radius',
+    '-khtml-border-radius',
+    'border-radius',
+
+    '-webkit-border-top-left-radius',
     '-webkit-border-top-right-radius',
     '-webkit-border-bottom-left-radius',
     '-webkit-border-bottom-right-radius',
@@ -114,124 +114,124 @@ $listing_proprietes = array(
     'border-top-right-radius',
     'border-bottom-left-radius',
     'border-bottom-right-radius',
-	
-	'overflow',
-	'overflow-x',
-	'overflow-y',
-	
+
+    'overflow',
+    'overflow-x',
+    'overflow-y',
+
 /* --------------------------------------------------------------------
-	Proprietes de texte
+    Proprietes de texte
    ----------------------------------------------------------------- */
-	
-	'azimuth',
-	'caption-side',
-	'clip',
-	'direction',
-	'empty-cells',
 
-	'table-layout',
-	
-	'text-align',
-	'text-decoration',
-	'text-indent',
-	'text-shadow',
-	'text-transform',
+    'azimuth',
+    'caption-side',
+    'clip',
+    'direction',
+    'empty-cells',
 
-	'font',
-	'font-family',
-	'font-size',
-	'font-style',
-	'font-variant',
-	'font-weight',
-	'line-height',
-	
-	'letter-spacing',
-	'white-space',
-	'word-spacing',
-	
-	'orphans',
-	'page-break-after',
-	'page-break-before',
-	'page-break-inside',
+    'table-layout',
 
-	'unicode-bidi',
-	'vertical-align',
-	'widows',
-	
-	'outline',
-	'outline-color',
-	'outline-style',
-	'outline-width',
-	
+    'text-align',
+    'text-decoration',
+    'text-indent',
+    'text-shadow',
+    'text-transform',
+
+    'font',
+    'font-family',
+    'font-size',
+    'font-style',
+    'font-variant',
+    'font-weight',
+    'line-height',
+
+    'letter-spacing',
+    'white-space',
+    'word-spacing',
+
+    'orphans',
+    'page-break-after',
+    'page-break-before',
+    'page-break-inside',
+
+    'unicode-bidi',
+    'vertical-align',
+    'widows',
+
+    'outline',
+    'outline-color',
+    'outline-style',
+    'outline-width',
+
 /* --------------------------------------------------------------------
-	Decoration
+    Decoration
    ----------------------------------------------------------------- */
-	
-	'list-style',
-	'list-style-image',
-	'list-style-position',
-	'list-style-type',
-	
-	'color',
-	'opacity',
-	
-	'background',
-	'background-attachment',
-	'background-color',
-	'background-image',
-	'background-position',
-	'background-repeat',
-	
-	'-webkit-background-size',
-	'-moz-background-size',
-	'background-size',
-	
-	'-webkit-background-clip',
-	'-moz-background-clip',
-	'background-clip',
-	
-	'-webkit-background-origin',
-	'-moz-background-origin',
-	'background-origin',
-	
-	'-webkit-box-shadow',
-	'-moz-box-shadow',
-	'box-shadow',
 
-	'-webkit-animation',
-	'-moz-animation',
-	'-o-animation',
-	'animation',
+    'list-style',
+    'list-style-image',
+    'list-style-position',
+    'list-style-type',
 
-/* --------------------------------------------------------------------
-	Transformation & Transitions
-	----------------------------------------------------------------- */
+    'color',
+    'opacity',
 
-	'-webkit-transition',
-	'-moz-transition',
-	'-ms-transition',
-	'-o-transition',
-	'transition',	
+    'background',
+    'background-attachment',
+    'background-color',
+    'background-image',
+    'background-position',
+    'background-repeat',
 
-	'-webkit-transform',
-	'-moz-transform',
-	'-ms-transform',
-	'-o-transform',
-	'transform',
+    '-webkit-background-size',
+    '-moz-background-size',
+    'background-size',
+
+    '-webkit-background-clip',
+    '-moz-background-clip',
+    'background-clip',
+
+    '-webkit-background-origin',
+    '-moz-background-origin',
+    'background-origin',
+
+    '-webkit-box-shadow',
+    '-moz-box-shadow',
+    'box-shadow',
+
+    '-webkit-animation',
+    '-moz-animation',
+    '-o-animation',
+    'animation',
 
 /* --------------------------------------------------------------------
-	Comportements
+    Transformation & Transitions
+    ----------------------------------------------------------------- */
+
+    '-webkit-transition',
+    '-moz-transition',
+    '-ms-transition',
+    '-o-transition',
+    'transition',
+
+    '-webkit-transform',
+    '-moz-transform',
+    '-ms-transform',
+    '-o-transform',
+    'transform',
+
+/* --------------------------------------------------------------------
+    Comportements
    ----------------------------------------------------------------- */
-	
-	'resize',
-	'cursor',
-	
-	'-webkit-user-select',
-	'-moz-user-select',
-	'user-select',
 
-	'-webkit-user-drag',
-	'-moz-user-drag',
-	'user-drag',
+    'resize',
+    'cursor',
+
+    '-webkit-user-select',
+    '-moz-user-select',
+    'user-select',
+
+    '-webkit-user-drag',
+    '-moz-user-drag',
+    'user-drag',
 
 );
