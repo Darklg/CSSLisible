c37750f432020fd7bb9f5a4e16a8e664cb2a1db7
Darklg <kev.rocher@gmail.com>
2013-05-24 13:54:51 +0200

    Unnecessary fix -> default behavior (using cookie values)

--- inc/header.php
+++ inc/header.php
@@ -27,11 +27,6 @@ $args['listing_proprietes'] = $listing_proprietes;
 
 $CSSLisible = new CSSLisible( $args );
 
-// @TOFIX : Dirty method to obtain real options below
-if ( isset( $args['csslisible_options'] ) ) {
-    $CSSLisible->options = array_merge( $CSSLisible->options, $args['csslisible_options'] );
-}
-
 if ( isset( $_POST['api'] ) || isset( $_GET['api'] ) ) {
     header( 'Content-type: text/css; charset=utf-8' );
     exit( $CSSLisible->buffer );
