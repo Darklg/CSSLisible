9947f14ddb500446fe6a1a190c32645ea406a8b2
Darklg <kev.rocher@gmail.com>
2013-05-25 19:29:45 +0200

    PHP:Tidy used on traduction.php

--- inc/traduction.php
+++ inc/traduction.php
@@ -15,15 +15,15 @@ $more_languages = array(
 );
 
 // Si la traduction en la langue demandée est disponible
-if(isset($_GET['lang']) && array_key_exists($_GET['lang'],$more_languages) && $more_languages[$_GET['lang']]['active']){
+if ( isset( $_GET['lang'] ) && array_key_exists( $_GET['lang'], $more_languages ) && $more_languages[$_GET['lang']]['active'] ) {
     $lang = $more_languages[$_GET['lang']]['lang'];
 }
 
 // On définit la langue
-putenv('LC_ALL=' . $lang);
-setlocale(LC_ALL, $lang);
+putenv( 'LC_ALL=' . $lang );
+setlocale( LC_ALL, $lang );
 
 // On charge la traduction
-bindtextdomain("CSSLisible", dirname(__FILE__)."/locale");
-bind_textdomain_codeset("CSSLisible", "UTF-8"); 
-textdomain("CSSLisible");
+bindtextdomain( "CSSLisible", dirname( __FILE__ )."/locale" );
+bind_textdomain_codeset( "CSSLisible", "UTF-8" );
+textdomain( "CSSLisible" );
