bb4189e5ef4fec1b2e49a7f19ec5a982ef296424
NumEricR <eric.github@gmail.com>
2013-05-27 23:58:13 +0200

    Avoid link on current language

--- inc/tpl/lang_menu.php
+++ inc/tpl/lang_menu.php
@@ -1,11 +1,11 @@
 <ul class="lang_menu"><?php
 foreach ( $more_languages as $id_lang => $llang ) {
-    $urlLang = '?lang=' . $id_lang;
-    if ( $id_lang == 'fr' ) {
-        $urlLang = '?';
-    }
-    echo '<li '.( $lang == $llang['lang'] ? 'class="current"' : '' ).'>'.
-        '<a href="' . $urlLang . '" lang="'.$id_lang.'" title="'.$llang['name'].'">'.$id_lang.'</a>'.
-        '</li>';
+	if ( $lang == $llang['lang'] ) {
+		$lang_text = '<span title="'.$llang['name'].'">'.$id_lang.'</span>';
+	}
+	else {
+		$lang_text = '<a href="?lang='.$id_lang.'" lang="'.$id_lang.'" title="'.$llang['name'].'">'.$id_lang.'</a>';
+	}
+	echo '<li'.( $lang == $llang['lang'] ? ' class="current"' : '' ).'>'.$lang_text.'</li>';
 }
 ?></ul>
