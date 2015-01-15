66012c4f40e44736992b627e8b7477b57c692d16
Darklg <kev.rocher@gmail.com>
2015-01-15 16:47:50 +0100

    New explanation link

--- inc/tpl/text.php
+++ inc/tpl/text.php
@@ -1,6 +1,6 @@
 <p>
     <?php echo _('CSSLisible va réindenter vos blocks de code,'); ?>
-    <a lang="fr" href="http://blog.goetter.fr/articles/ordonnez-vos-declarations-css/" rel="external" target="_blank"><?php echo _('ordonner vos propriétés'); ?></a>,
+    <a lang="fr" href="http://blog.goetter.fr/2011/12/20/ordonnez-vos-declarations-css/" rel="external" target="_blank"><?php echo _('ordonner vos propriétés'); ?></a>,
     <?php echo _('afin de fournir un code CSS Lisible et plus maintenable. '); ?>
     <?php echo _('Attention, les commentaires internes aux sélecteurs sont retirés !'); ?>
 </p>
